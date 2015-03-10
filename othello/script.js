19ad3be244d96c9485c7bda76d816b01b9516117
Kota Nishinaka <e1152107@soka-u.jp>
2015-03-10 22:58:53 +0900

    simple othello

--- othello/script.js
+++ othello/script.js
@@ -2,11 +2,12 @@
   var SIZE = 4,  //TODO: enlarge later
 WHITE = 'white',
 EMPTY = 'empty',
-BLACK = 'black',
-board = {};
+BLACK = 'black';
 
 
 function initBoard() {
+  var board = {};
+
   for (var x = 0; x < SIZE; x++) {
     for (var y = 0; y < SIZE; y++) {
       board[[x,y]] = EMPTY;
@@ -16,6 +17,7 @@ function initBoard() {
   board[[(SIZE/2)-1,(SIZE/2)-1]] = WHITE;
   board[[SIZE/2,(SIZE/2)-1]] = BLACK;
   board[[(SIZE/2)-1,SIZE/2]] = BLACK;
+
   return board;
 }
 
@@ -23,21 +25,21 @@ function drawBoard(board, player) {
   var ss = [];
   ss.push('<table>');
 
-  for (var i = -1; i < SIZE; i++) {
+  for (var y = -1; y < SIZE; y++) {
     ss.push('<tr>');
-    for (var j = -1; j < SIZE; j++) {
-      if (i > -1 && j > -1) {
+    for (var x = -1; x < SIZE; x++) {
+      if (x > -1 && y > -1) {
         ss.push('<td class="');
         ss.push('mas');
         ss.push(' ');
-        ss.push(board[[i,j]]);
+        ss.push(board[[x,y]]);
         ss.push('">');
         ss.push('<span class="circle"></span>');
         ss.push('</td>');
-      }else if (i === -1 && j > -1) {
-        ss.push('<th>'+ 'abcdefgh'[j] + '</th>');
-      }else if (i > -1 && j === -1) {
-        ss.push('<th>'+ '12345678'[i]+ '</th>');
+      }else if (y === -1 && x > -1) {
+        ss.push('<th>'+ 'abcdefgh'[x] + '</th>');
+      }else if (y > -1 && x === -1) {
+        ss.push('<th>'+ '12345678'[y]+ '</th>');
       }else {
         ss.push('<th></th>');
       }
@@ -74,7 +76,9 @@ function completePassingMove(attackingMoves, board, player, wasPassed) {
   }else if(!wasPassed) {
     return [{
       isPassingMove: true,
-        gameTree: createGameTree(board, nextPlayer(player), true)
+        gameTreePromise: delay(function () {
+          return createGameTree(board, nextPlayer(player), true);
+        })
     }];
   } else {
     return [];
@@ -90,11 +94,18 @@ function listAttackingMoves(board, player) {
         moves.push({
           x:x,
           y:y,
-          gameTree: createGameTree(
-            createAttackedBoard(board, x, y, player),
-            nextPlayer(player),
-            false
-            )
+          gameTreePromise: (function (x,y) {
+            return delay(function () {
+              return createGameTree(
+                createAttackedBoard(board, x, y, player),
+                nextPlayer(player),
+                false
+                );
+
+            });
+          })(x,y)
+
+
         });
       }
     }
@@ -142,8 +153,8 @@ function listVulnerableCells(board, x, y, player) {
         if (cell === player && 2 <= i) {
           for (j = 0; j < i; j++) {
             vulnerableCells.push([x + j * dx, y + j * dy]);
-            break;
           }
+          break;
         }
         if (cell !== opponent) {
           break;
@@ -159,7 +170,7 @@ function setUpUIToChooseMove(gameTree) {
   gameTree.moves.forEach(function (m,i) {
     $('#console').append(
       $('<input type="button" class="btn">').val(createLabelForMove(m)).click(function () {
-        shiftToNewGameTree(m.gameTree);
+        shiftToNewGameTree(force(m.gameTreePromise));
       })
       );
   });
@@ -198,7 +209,7 @@ function setUpUIToReset() {
       $('<input type="button" class="btn">').val('Start a new game').click(function() {
         resetGame();
       })
-  );
+      );
 }
 
 function resetGame() {
@@ -216,9 +227,26 @@ function shiftToNewGameTree(gameTree) {
   }
 }
 
-resetGame();
-})();
+function delay(expressionAsFunction) {
+  var result;
+  var isEvaluated = false;
 
+  return function() {
+    if (!isEvaluated) {
+      result = expressionAsFunction();
+      isEvaluated = true;
+    }
+    return result;
+  };
+}
+
+function force(promise) {
+  return promise();
+}
+
+resetGame();
+
+})();
 
 
 
